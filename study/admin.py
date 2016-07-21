70a1c416701e328a5c3ee6b8e0e5bd367d55efda
LeeSungOn <dltjddhs@naver.com>
2016-07-21 22:27:30 +0900

    study/admin.py 에 modles.py에서 정의한 클래스들 등록

--- study/admin.py
+++ study/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from study.models import *
 
-# Register your models here.
+
+admin.site.register(Study)
+admin.site.register(StudyMember)
+admin.site.register(StudyLike)
+admin.site.register(StudyNotice)
+admin.site.register(Calender)
+admin.site.register(CalenderCategory)
+admin.site.register(HomeWork)
+admin.site.register(HomeWorkComment)
+admin.site.register(HomeWorkCommentFile)
+admin.site.register(HomeWorkFile)
+admin.site.register(HomeWorkReComment)
+admin.site.register(Reference)
+admin.site.register(ReferenceComment)
+admin.site.register(ReferenceFile)
+admin.site.register(Question)
+admin.site.register(QuestionComment)
+admin.site.register(QuestionFile)
