3a58fee1a53ffba5b72ac039de9ddace286226fc
LeeSungOn <dltjddhs@gmail.com>
2016-08-30 21:34:26 +0900

    UPDATE - remove verification/views.py typing error

--- verification/views.py
+++ verification/views.py
@@ -38,9 +38,9 @@ class VerificationViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-
-
-        Verification.object.create()
+        #
+        #
+        # Verification.object.create()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
